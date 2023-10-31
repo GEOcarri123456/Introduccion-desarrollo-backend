34db471fbd58d2ba1148e2b8771aa41e75e107c6
GEOcarri123456 <2202004@utrivieramaya.edu.mx>
2023-10-31 08:27:27 -0500

    first commit

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+var cadena = "Buenos, dias";
+var numero = 42;
+var booleano = true;
+var arreglo = [1, 2, 3];
+var objeto = { nombre: "Geovani", edad: 19 };
+var nulo = null;
+var noDefinido;
+
+console.log(typeof cadena);   // "string"
+console.log(typeof numero);   // "number"
+console.log(typeof booleano); // "boolean"
+console.log(typeof arreglo);  // "object"
+console.log(typeof objeto);   // "object"
+console.log(typeof nulo);     // "object" (un error común en JavaScript)
+console.log(typeof noDefinido); // "undefined"
---
+++ variable.js
@@ -0,0 +1,43 @@
+var nombre = "Geovani";
+var esMayorDeEdad = true;
+var variableIndefinida;
+var valorNulo = null;
+
+console.log(typeof nombre);          // "string"
+console.log(typeof esMayorDeEdad);   // "boolean"
+console.log(typeof variableIndefinida); // "undefined"
+console.log(typeof valorNulo);       // "object" (un error común en JavaScript)
+
+
+var variable1;
+var variable2;
+var variable3;
+var variable4;
+
+var nombre = "Geovani";
+var esMayorDeEdad = true;
+var variableIndefinida;
+var valorNulo = null;
+
+var nombre = "Geovani";
+var apellido = "Carrillo";
+var estadoCivil = "Soltero";
+var pais = "México";
+var edad = 19;
+
+var persona = {
+    nombre: "Geovani",
+    apellido: "Carrillo",
+    estadoCivil: "Soltero",
+    país: "Mexico",
+    edad: 19
+  };
+
+  var myAge = 19;
+var yourAge = 20;
+
+console.log("Mi edad es: " + myAge);
+console.log("Tu edad es: " + yourAge);
+
+  
+
